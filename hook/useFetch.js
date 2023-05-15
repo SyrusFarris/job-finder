a93df2680812082fe3312622f18a45bd13780358
Syrus <syrusfarris@gmail.com>
2023-05-15 15:06:04 -0500

    connect the jsearch api to the application. Going to start on the popularjob card component soon

---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endpoint, query) => {
+    const [data, setdata] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+          'X-RapidAPI-Key': '5dc42ba75emshbee75def953eef6p11ccd1jsn81691632d244',
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+      };
+
+      const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request (options);
+
+            setdata(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('There is an error')
+        } finally {
+            setIsLoading(false);
+        }
+      }
+
+      useEffect(() => {
+        fetchData();
+      }, []);
+
+      const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+      }
+
+      return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
